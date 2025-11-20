0073bfd129c685687a6a5279484bdfe1cc4927b1
danilasol12-source <danilasol12@mail.ru>
2025-11-21 01:07:52 +0300

    Add files via upload

---
+++ PR5_Solntsev/src/MovableCircle.java
@@ -0,0 +1,33 @@
+public class MovableCircle implements Movable {
+    private MovablePoint center;
+    private int radius;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    public String toString() {
+        return "MovableCircle [radius=" + radius + ", center=" + center + "]";
+    }
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+}
---
+++ PR5_Solntsev/src/MovableRectangle.java
@@ -0,0 +1,51 @@
+public class MovableRectangle implements Movable {
+    private MovablePoint topLeft;
+    private MovablePoint bottomRight;
+
+    public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed) {
+        this.topLeft = new MovablePoint(x1, y1, xSpeed, ySpeed);
+        this.bottomRight = new MovablePoint(x2, y2, xSpeed, ySpeed);
+    }
+
+    public String toString() {
+        return "MovableRectangle{" + "topLeft=" + topLeft + ", bottomRight=" + bottomRight + "}";
+    }
+
+    public boolean SameSpeed(){
+        return (topLeft.getxSpeed() == bottomRight.getxSpeed()) && (topLeft.getySpeed() == bottomRight.getySpeed());
+    }
+
+    @Override
+    public void moveUp(){
+        if (SameSpeed()){
+            topLeft.moveUp();
+            bottomRight.moveUp();
+        }
+    }
+
+    @Override
+    public void moveDown(){
+        if (SameSpeed()){
+            topLeft.moveDown();
+            bottomRight.moveDown();
+        }
+    }
+
+    @Override
+    public void moveLeft(){
+        if (SameSpeed()){
+            topLeft.moveLeft();
+            bottomRight.moveLeft();
+        }
+    }
+
+    @Override
+    public void moveRight(){
+        if (SameSpeed()){
+            topLeft.moveRight();
+            bottomRight.moveRight();
+        }
+    }
+}
+
+
